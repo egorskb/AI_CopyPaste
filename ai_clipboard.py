6f7b5a322cb7464e62c7fc129afe190bbe0d0cfe
Egor <egorskalozub2003@gmail.com>
2023-04-27 19:33:07 +0300

    created logging for debugging

--- ai_clipboard.py
+++ ai_clipboard.py
@@ -1,8 +1,26 @@
+import os
+import logging
 import openai
 
 from settings_manager import get_api_key, load_settings
 
 openai.api_key = f"{get_api_key()}"
+
+
+log_dir = "logs"
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+log_filename = os.path.join(log_dir, "application.log")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] - %(message)s",
+    handlers=[
+        logging.FileHandler(log_filename),
+        logging.StreamHandler()
+    ]
+)
 
 
 def ask_openai(question):
@@ -17,4 +35,9 @@ def ask_openai(question):
         presence_penalty=0,
     )
     answer = response.choices[0].text.strip()
+    break_line = "=" * 40
+    print(break_line)
+    logging.info(f"Question: {question}")
+    logging.info(f"Response: {response}")
+    logging.info(f"Answer: {answer}")
     return answer
